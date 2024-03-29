56507cb46eea31073e4d88e74df9165fcf6dafef
linshio <3361445513@qq.com>
2024-03-29 17:34:02 +0800

    update

---
+++ Vue组件化编程/02单文件组件/School.vue
@@ -0,0 +1,46 @@
+<template lang="">
+	<div class="demo">
+		<h2>学校名称：{{ name }}</h2>
+		<h2>学校地址：{{address}}</h2>
+		<button @click="showName">button</button>
+	</div>
+</template>
+<script>
+	//全写
+	// const school = Vue.extend({
+	// 	data() {
+	// 		return {
+	// 		name: 'azSchool',
+	// 		address: 'qwer'
+	// 		}
+	// 	},
+	// 	methods: {
+	// 		showName() {
+	// 		alert(this.x)
+	// 		}
+	// 	}
+	// })
+	// //暴露模块的方式 => 默认暴露
+	// export default school
+
+	//简写方式
+	export default {
+		name:'School',
+		data() {
+			return {
+			name: 'azSchool',
+			address: 'qwer'
+			}
+		},
+		methods: {
+			showName() {
+			alert(this.name)
+			}
+		}
+	}
+</script>
+<style lang="">
+    .demo{
+			background-color: white;
+		}
+</style>
