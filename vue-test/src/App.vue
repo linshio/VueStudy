d18b7d6369039860e41fc414bfb55ddc07a22ec8
linshio <3361445513@qq.com>
2024-04-07 17:15:22 +0800

    update

---
+++ vue-test/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="root">
+    <button @click="getStudents()">获取学生的信息</button>
+    <button @click="getCars()">获取汽车的信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name:'App',
+  methods: {
+    getStudents(){
+      axios.get('http://localhost:8080/student/students').then(
+        response => {
+          if(response.status===200)
+            console.log('请求成功，返回的数据为==>',response.data);
+        },
+        error => {
+          console.log('请求失败，错误的信息为==>',error.message);
+        }
+      )
+    },
+    getCars(){
+      axios.get('http://localhost:8080/car/cars').then(
+        response=>{
+          console.log('请求成功，返回的数据为==>',response.data);
+        },
+        error=>{
+          console.log('请求失败，错误的信息为==>',error.message);
+        }
+      )
+    }
+  },
+}
+</script>
--- vue-test/vue.config.js
+++ vue-test/vue.config.js
@@ -1,5 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave:false,//关闭语法检查
-  transpileDependencies: true
+  transpileDependencies: true,
+  //开启代理服务器(方式一)
+  // devServer:{
+  //   proxy: 'http://localhost:5000'
+  // }
+  //开启代理服务器(方式二) 可以配置多个代理
+  devServer:{
+    proxy: {
+      '/student':{ //匹配所有以/student开头的请求路径
+        target: 'http://localhost:5000', //代理目标的真实路径
+        pathRewrite:{'^/student':''},
+        changeOrigin:true //是否假装一下真实的服务器地址
+      },
+      '/car':{ //匹配所有以/car开头的请求路径
+        target: 'http://localhost:5001', //代理目标的真实路径
+        pathRewrite:{'^/car':''}
+      }
+    }
+  }
 })
