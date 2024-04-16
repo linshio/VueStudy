a3a79945521be7b78d6bbb45fd80e273dbbc36ff
H-ling-end <3361445513@qq.com>
2024-04-16 20:28:35 +0800

    update

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -8,14 +8,20 @@
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
-          <a class="list-group-item active" href="./about.html">About</a>
-          <a class="list-group-item" href="./home.html">Home</a>
+          <!-- 原始html中我们使用a标签实现页面的跳转 -->
+          <!-- <a class="list-group-item active" href="./about.html">About</a>
+          <a class="list-group-item" href="./home.html">Home</a> -->
+
+          <!-- Vue中借助router-link 标签实现路由的切换 -->
+          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+          <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
         </div>
       </div>
       <div class="col-xs-6">
         <div class="panel">
           <div class="panel-body">
-            <h2>我是About的内容</h2>
+            <!-- 指定组件的呈现位置 -->
+            <router-view></router-view>
           </div>
         </div>
       </div>
@@ -25,11 +31,7 @@
 
 <script>
 export default {
-  name:'App',
-  components:{},
-  mounted() {
-    
-  },
+  name:'App'
 }
 </script>
 
