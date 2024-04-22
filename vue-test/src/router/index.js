5e8008f54fa71d3683531d622e6720b0ee2622ee
linshio <3361445513@qq.com>
2024-04-22 12:00:03 +0800

    update

--- vue-test/src/router/index.js
+++ vue-test/src/router/index.js
@@ -2,6 +2,9 @@
 import VueRouter from 'vue-router'
 import About from '../pages/About.vue' 
 import Home from '../pages/Home.vue'
+import Message from '../pages/Message.vue'
+import News from '../pages/News.vue'
+import Detail from '../pages/Detail.vue'
 //创建并暴露一个路由器
 export default new VueRouter({
   routes:[
@@ -11,7 +14,23 @@ export default new VueRouter({
     },
     {
       path:'/home',
-      component:Home
+      component:Home,
+      children:[
+        {
+          path:'message',
+          component:Message,
+          children:[
+            {
+              path:'detail',
+              component:Detail  
+            }
+          ]
+        },
+        {
+          path:'news',
+          component:News
+        }
+      ]
     }
   ]
-})
+}) 
